02e211069e9f12dd5a31a5987fa0b02a70780765
Jacobp60 <Perezj4801@gmail.com>
2022-09-21 18:33:37 -0500

    A

---
+++ Models/Turtle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractExample.Models
+{
+    public class Turtle : Animal
+    {
+        public Turtle(string name)
+        {
+            Name = name;
+        }
+
+        public Turtle()
+        {
+            Name = "Master Oogway";
+        }
+        public override void MakeNoise()
+        {
+            Console.WriteLine($"{Name} make the noise click click cluck ");
+        }
+
+        public virtual void Sleep()
+        {
+            Console.WriteLine("ZZZZZZ");
+        }
+    }
+}
